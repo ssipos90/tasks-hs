e6ea49b811de1147e48eb0f3e16e75a814306ef3
Sebastian Sipos <sebastiansipos90@gmail.com>
2019-09-04 23:48:11 +0300

    WIP on megaparsec. WIP on error handling

--- src/App.hs
+++ src/App.hs
@@ -7,21 +7,24 @@ module App where
   import           Protolude
   import           Control.Monad                  ((>=>))
   import           Data.Char                      ( digitToInt )
-  import           Data.Either                    ( isLeft, fromLeft, fromRight )
+  import           Data.Either                    ( isLeft, fromLeft, fromRight, rights )
   import           Data.List                      ( (!!) )
   import qualified Data.Text                      as T
   import           Data.Text.IO                   ( readFile )
   import           Data.Text.Read                 ( decimal )
+  import           Data.Void                      ( Void )
   import           System.IO                      (IO)
+  import qualified Text.Megaparsec                as TM
+  import qualified Text.Megaparsec.Char           as TMC
   
-  data ActionSuccess = ActionSuccess Text [Todo]
+  type Parser = TM.Parsec Void Text
   type Error = Text
   type ActionFn = [Todo] -> Text -> Either Error ActionSuccess
-  
+
+  data ActionSuccess = ActionSuccess Text [Todo]
   data Todo = Todo { todoTask :: Text
                    , todoDone :: Bool
                    } deriving (Show)
-  
   data Action = Action { actionCommand :: Text
                        , actionFn :: ActionFn
                        , actionExample :: Text
@@ -42,13 +45,14 @@ module App where
   showDone :: Bool -> Text
   showDone False = " "
   showDone True = "x"
-  
-  parseTodo :: Text -> Todo
-  parseTodo str = Todo task (d == '1')
+
+  parseTodo :: Text -> Either Error Todo
+  parseTodo str = Right (Todo task (d == '1'))
             where d = T.head str
                   task = T.drop 2 str
+                  -- p = TM.runParser TMC.binDigitChar "123"
   
-  parseTodos :: Text -> [Todo]
+  parseTodos :: Text -> [Either Error Todo]
   parseTodos = map parseTodo . T.lines
   
   create :: [Todo] -> Text -> Either Error ActionSuccess
@@ -56,10 +60,11 @@ module App where
   
   toggle :: Bool -> [Todo] -> Text -> Either Error ActionSuccess
   toggle _ [] _ = Left "Error, empty list, bro!"
-  toggle as todos nStr = case decimal nStr of
-      Left e -> Left (T.pack e)
-      Right(n,_) -> fmap (ActionSuccess "E gata, ah?") (updateTodoAt n (markTodo as) todos)
-  
+  toggle as todos nStr = either
+                          (Left . T.pack)
+                          (\(n, _) -> ActionSuccess "Finished, ah?" <$> updateTodoAt n (markTodo as) todos)
+                          (decimal nStr)
+
   mark = toggle True
   unmark = toggle False
   
@@ -67,15 +72,18 @@ module App where
   markTodo as (Todo task done)
     | done == as = Left "Noop!"
     | otherwise = Right Todo { todoTask=task, todoDone=as }
-    
+
   updateTodoAt :: Int -> (Todo -> Either Error Todo) -> [Todo] -> Either Error [Todo]
-  updateTodoAt k fn todos
+  updateTodoAt position fn todos
                   | n < 0 = Left "Must be a strict positive, bruh!"
                   | n >= length todos = Left "Out of bounds?"
-                  | otherwise = case fn (todos!!n) of 
-                          Left e -> Left e
-                          Right todo -> Right(take n todos ++ [todo] ++ drop (n+1) todos)
-                    where n = k - 1;
+                  | otherwise = fn (todos!!n)
+                                >>= (\todo -> Right(take n todos ++ [todo] ++ drop (n+1) todos))
+                  where n = position - 1
+
+  fuu :: Bool -> Error -> Maybe Error
+  fuu True e = Just e
+  fuu False _ = Nothing
 
   showAction :: Action -> Text
   showAction Action { actionCommand, actionExample } =
@@ -83,8 +91,8 @@ module App where
   
   parseAction :: [Action] -> Text -> (Text, Maybe Action)
   parseAction _ "" = ("", Nothing)
-  parseAction actions str = (T.concat rest, find (\Action {actionCommand} -> command == actionCommand) actions)
-    where (command : rest) = T.words str 
+  parseAction actions str = (T.concat args, find (\Action {actionCommand} -> actionCommand == command) actions)
+    where (command : args) = T.words str 
   
   runAction :: Maybe Action -> [Todo] -> Text -> IO [Todo]
   runAction Nothing todos _ = runActionHelper "Invalid action" todos
@@ -97,15 +105,17 @@ module App where
   runActionHelper :: Text -> [Todo] -> IO [Todo]
   runActionHelper s todos = putStrLn s >> return todos
   
-  runner :: ([Todo] -> IO ()) -> [Todo] -> IO ()
-  runner saveTodos todos = do
+  runner :: ([Todo] -> IO ()) -> [Either Error Todo] -> IO ()
+  runner saveTodos eitherTodos = do
+    -- TODO make sure to show the errors at some point :)
+    let todos = rights eitherTodos
     putStrLn $ T.concat ["\n", showTodos todos]
     putStrLn $ T.unlines $ map showAction actions
     command <- getLine
     let (params, action) = parseAction actions command
     newTodos <- runAction action todos params
     saveTodos newTodos
-    runner saveTodos newTodos
+    runner saveTodos $ map Right newTodos
     where
       actions =
         [ Action "create"   create   "create Fuuuu your mum...."
