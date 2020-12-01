c886522f494d82031482672ab1bff1ea67fcbba3
Sebastian Sipos <sebastiansipos90@gmail.com>
2020-12-01 13:34:45 +0200

    MVP for creating a task

--- src/Main.hs
+++ src/Main.hs
@@ -1,28 +1,26 @@
-{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import App
-import Lens.Micro ( (^.), over )
-import Lens.Micro.TH ( makeLensesFor )
 import qualified Brick.AttrMap as A
-import qualified Brick.Main as M
-import qualified Brick.Forms as F
 import Brick.Forms ((@@=))
+import qualified Brick.Forms as F
+import qualified Brick.Main as M
 import qualified Brick.Types as T
 import qualified Brick.Util as U
 import qualified Brick.Widgets.Border as B
 import qualified Brick.Widgets.Center as C
 import Brick.Widgets.Core
-  ( hLimit,
+  ( fill,
+    hLimit,
+    padBottom,
     str,
     vBox,
     vLimit,
     withAttr,
-    padBottom,
-    fill,
     (<+>),
   )
 import qualified Brick.Widgets.List as L
@@ -30,42 +28,53 @@ import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text as Tx
 import qualified Data.Vector as Vec
 import qualified Graphics.Vty as V
+import Lens.Micro (over, (^.))
+import Lens.Micro.TH (makeLensesFor)
 import Protolude
 
 defaultFile :: FilePath
 defaultFile = "tasks.json"
 
+data TaskField
+  = NameField
+  | DescriptionField
+  deriving (Eq, Ord, Show)
 
-data TaskField = NameField
-               | DescriptionField
-               deriving (Eq, Ord, Show)
-type CreateForm = F.Form Task () ()
-data AppPage = CreatePage CreateForm | ListPage | DetailPage
+type CreateForm e = F.Form Task e ()
 
-data AppState = AppState
-  { tasks :: L.List () Task
-  , page :: AppPage
+data AppPage e = CreatePage (CreateForm e) | ListPage | DetailPage
+
+data AppState e = AppState
+  { tasks :: L.List () Task,
+    page :: AppPage e
   }
 
 makeLensesFor [("tasks", "_tasks"), ("page", "_page")] ''AppState
 
-mkCreateForm :: Task -> CreateForm
-mkCreateForm = F.newForm [ label "Work" @@= F.editTextField _title () (Just 1) 
-                         ]
-               where label s w = padBottom (T.Pad 1) $
-                        (vLimit 1 $ hLimit 15 $ str s <+> fill ' ') <+> w 
+mkCreateForm :: Task -> CreateForm e
+mkCreateForm =
+  F.newForm
+    [ label "Work" @@= F.editTextField _title () (Just 1)
+    ]
+  where
+    label s w =
+      padBottom (T.Pad 1) $
+        vLimit 1 (hLimit 15 $ str s <+> fill ' ') <+> w
 
-taskToItem task = (case isTaskDone task of
-                    True -> withAttr completedTask $ str " ✔ "
-                    False -> str "   "
-                  )
-              <+> (str $ Tx.unpack $ task ^. _title)
+taskToItem task =
+  taskCheckMark
+    <+> str (Tx.unpack $ task ^. _title)
+  where
+    taskCheckMark =
+      if (isTaskDone task)
+        then withAttr completedTask $ str " ✔ "
+        else str "   "
 
-ui :: AppState -> [T.Widget ()]
+ui :: AppState e -> [T.Widget ()]
 ui s =
   case s ^. _page of
     ListPage -> [L.renderList (\_ task -> taskToItem task) True (s ^. _tasks)]
-    CreatePage f -> [ F.renderForm f ]
+    CreatePage f -> [F.renderForm f]
 
 completedTask = A.attrName "completed"
 
@@ -73,47 +82,55 @@ attributeMap :: A.AttrMap
 attributeMap =
   A.attrMap
     V.defAttr
-    [ (L.listSelectedAttr, U.fg V.blue)
-    , (completedTask, U.fg V.green) 
+    [ (L.listSelectedAttr, U.fg V.blue),
+      (completedTask, U.fg V.green)
     ]
 
-createPageEvent :: AppState -> T.BrickEvent () e -> CreateForm -> T.EventM () (T.Next AppState)
-createPageEvent s e f = case e of
-      T.VtyEvent (V.EvKey V.KEsc []) -> M.halt s
-      e -> do
-        let nf = F.handleFormEvent e f
-        M.continue $ over _page (\_ -> nf) s
+initialTask =
+  Task
+    { title = "",
+      addedAt = 0,
+      finishedAt = Nothing
+    }
 
 listPageEvent s (T.VtyEvent e) = case e of
-      V.EvKey (V.KChar 'c') [] -> M.continue $ over _page (\_ -> 
-                                    let initialTask = Task { title = ""
-                                                            , addedAt = 0
-                                                            , finishedAt = Nothing
-                                                            }
-                                    in CreatePage $ mkCreateForm initialTask
-                             ) s
-      V.EvKey (V.KChar 'j') [] -> M.continue $ over _tasks L.listMoveDown s
-      V.EvKey (V.KChar 'k') [] -> M.continue $ over _tasks L.listMoveUp s
-      V.EvKey (V.KChar 'g') [] -> M.continue $ over _tasks (L.listMoveTo 0) s
-      V.EvKey (V.KChar 'G') [] ->
-        M.continue $ over _tasks (L.listMoveTo $ length $ s ^. _tasks) s
-      V.EvKey (V.KChar _) [] -> M.continue s
-      V.EvKey _ []-> M.continue s
+  (V.EvKey V.KEsc []) -> M.halt s
+  V.EvKey (V.KChar 'c') [] ->
+    M.continue $
+      over
+        _page
+        (\_ -> CreatePage $ mkCreateForm initialTask)
+        s
+  e -> do
+    tasks <- L.handleListEventVi L.handleListEvent e (s ^. _tasks)
+    M.continue $ over _tasks (const tasks) s
 
-appEvent :: AppState -> T.BrickEvent () e -> T.EventM () (T.Next AppState)
-appEvent s e =
+createPageHandler :: AppState e -> CreateForm e -> T.BrickEvent () e -> T.EventM () (T.Next (AppState e))
+createPageHandler s f e = case e of
+  T.VtyEvent (V.EvKey V.KEsc []) -> M.halt s
+  T.VtyEvent (V.EvKey V.KEnter []) -> do
+    let tasks = s ^. _tasks
+    M.continue $ s { tasks = L.listInsert (Vec.length $ L.listElements tasks) (F.formState f) tasks
+                   , page = ListPage
+                   }
+  _ -> do
+    nf <- F.handleFormEvent e f
+    M.continue $ over _page (\_ -> CreatePage nf) s
+
+handleEvent :: AppState e -> T.BrickEvent () e -> T.EventM () (T.Next (AppState e))
+handleEvent s e =
   case e of
-    T.VtyEvent (V.EvKey V.KEsc []) -> M.halt s
-    e -> case s ^. _page of
-            CreatePage f -> createPageEvent e f
-            ListPage -> listPageEvent s e
+    T.VtyEvent (V.EvResize {}) -> M.continue s
+    _ -> case s ^. _page of
+      CreatePage f -> createPageHandler s f e
+      ListPage -> listPageEvent s e
 
-app :: M.App AppState e ()
+app :: M.App (AppState e) e ()
 app =
   M.App
     { M.appDraw = ui,
       M.appChooseCursor = M.showFirstCursor,
-      M.appHandleEvent = appEvent,
+      M.appHandleEvent = handleEvent,
       M.appStartEvent = return,
       M.appAttrMap = const attributeMap
     }
